5fae3099026c300d0d0c5fc9dc47831f565ef18c
xionSM <be-li-ath@hotmail.com>
2022-01-01 14:43:48 +0200

    Exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    if (num < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; num > 0; num--) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;

8731e8cc39879e6caa2532c7780d1d4f7abc1bb5
xionSM <be-li-ath@hotmail.com>
2022-01-08 17:56:58 +0200

    Removes from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...value) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++){
+        
+        let shallKeep = true;
+        
+        for (let x = 0; x < value.length; x++) {
+            if (value[x] === array[i]) {
+                shallKeep = false
+            }
+        }
+        if (shallKeep) {
+            newArray.push(array[i])
+        } 
 
+        }
+    
+    
+    return newArray
 };
 
 // Do not edit below this line

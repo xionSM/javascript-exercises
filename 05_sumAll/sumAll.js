19c92f1fba5d77a4459f953991fdf79c9d095b57
xionSM <be-li-ath@hotmail.com>
2022-01-09 17:27:51 +0200

    Sum numbers within the range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let n = int1;
+    let x = 0;
+    if (int1 < 0) {
+        return "ERROR"
+    } else if (int2 < 0) {
+        return "ERROR"
+    }
+    if (typeof int1 !== 'number') {
+        return "ERROR"
+    } else if (typeof int2 !== 'number') {
+        return "ERROR"
+    }
+ 
+    if (int1 > int2) {
+        for (; n >= int2; n--){
+            x += n
+        }
+    } else {
+        for (; n <= int2; n++){
+            x += n
+        }
+        
+    }
+    return x
 };
 
 // Do not edit below this line

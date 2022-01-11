2e1b60d0082191dd65a71ae2ede09dcec71a8da4
xionSM <be-li-ath@hotmail.com>
2022-01-11 17:59:08 +0200

    Round Celsius to Fahrenheit and viceversa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+let fahToCel = (fahrenheit - 32) / 1.8;
+let roundedFah = Math.round(fahToCel * 10) / 10;
+return roundedFah
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+let celToFah = (celsius * 1.8) + 32;
+let roundedCel = Math.round(celToFah * 10) / 10;
+return roundedCel
 };
 
 // Do not edit below this line

9aebdd73143823e636be5999dbad22afdcb23066
xionSM <be-li-ath@hotmail.com>
2022-01-11 21:31:16 +0200

    Check if a word is Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+let re = /([.,!])/gi;
+let newString = string.replace(re, "");
+let newArray = newString.split("");
+let reverseArray = newArray.reverse();
+let arrayJoin = reverseArray.join('');
+let arrayReplace = arrayJoin.replace(/ /g, "");
+console.log(arrayReplace);
+let stringReplace = newString.replace(/ /g, "");
+console.log(stringReplace);
 
+if (stringReplace.toLowerCase() === arrayReplace.toLowerCase()) {
+    return true
+} else {
+    return false
+}
 };
 
 // Do not edit below this line

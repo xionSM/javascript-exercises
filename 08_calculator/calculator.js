1ef7e46eced729039aecfc70f773463704a74709
xionSM <be-li-ath@hotmail.com>
2022-01-11 19:25:24 +0200

    Add, Sum, Multiply, Power and Factorial Numbers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
+const add = function(num1, num2) {
+let sum = num1 + num2;
+return sum
+};
+
+const subtract = function(num1, num2) {
+let sub = num1 - num2; 
+return sub
+};
+
+const sum = function(array) {
+let total = 0;
+for (let i in array) {
+  total += array[i]
+}
+return total
+};
+
+const multiply = function(array) {
+let sum = 1;
+  for (let i = 0; i < array.length; i++) {
+  sum *= array[i]
+}
+return sum
+};
+
+const power = function(num, power) {
+let sum = 1;
+  for (let i = 0; i < power; i++) {
+   sum *= num
+}
+return sum
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
+  let newArray = [];
+  let sum = 1;
+  for (let i = num; num > 0; num--){
+    newArray.push(i)
+    sum *= newArray.length
+  }
+  return sum
 	
 };
 

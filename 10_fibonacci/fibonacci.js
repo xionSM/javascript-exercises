ff12eba905d1eced103f4a1e7b74441ab77c2dad
xionSM <be-li-ath@hotmail.com>
2022-01-12 20:13:00 +0200

    Show a number from the fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+if (n < 0) {
+    return "OOPS"
+}
+else if (n <= 2 ){
+    return 1
+} else {
+    return fibonacci(n-1) + fibonacci(n-2);
+}
 };
 
 // Do not edit below this line

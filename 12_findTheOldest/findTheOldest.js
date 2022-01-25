90bbe3ea4d58bcce0728542f260c1bdda1b23a2c
xionSM <be-li-ath@hotmail.com>
2022-01-25 19:08:54 +0200

    Find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = (obj) => {
+    let oldest = obj.sort((a, b) => {
+        let aYearOfD = a.yearOfDeath || new Date().getFullYear();
+        let bYearOfD = b.yearOfDeath || new Date().getFullYear();
+        let aAge = aYearOfD - a.yearOfBirth;
+        let bAge = bYearOfD - b.yearOfBirth;
+        return bAge - aAge
+    }) 
+    console.log(oldest)
+    return oldest[0]
 };
 
 // Do not edit below this line
